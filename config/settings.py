692851d4d25c07c7048248f7e8f5afd147750087
RAJESH36-IN <RAJESH36-IN@users.noreply.github.com>
2026-07-20 00:10:33 +0530

    Remove deprecated ssl_require keyword argument from dj_database_url.config

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,8 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 
 
